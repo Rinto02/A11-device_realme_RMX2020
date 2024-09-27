f10334233b7e2e5c1204d26abdf1b4d4d79223c6
凛岡村リント <138510229+Rinto02@users.noreply.github.com>
2024-09-27 21:22:38 +0600

    RMX2020: Bringup DotOS
    Signed-off-by: 凛岡村リント <138510229+Rinto02@users.noreply.github.com>

--- dot_RMX2020.mk
+++ dot_RMX2020.mk
@@ -21,17 +21,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_RMX2020
+PRODUCT_NAME := dot_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := Realme G70 Series
+PRODUCT_MODEL := Realme C3
 PRODUCT_MANUFACTURER := realme
 
 TARGET_BOOT_ANIMATION_RES := 720
+WITH_GAPPS := true
 
 # Build info
 BUILD_FINGERPRINT := "google/coral/coral:11/RQ3A.210905.001/7511028:user/release-keys"
